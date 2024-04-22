9d41430ada05d061f3ef67527e1ace944cb2f06a
Jonathan Santos <jwsantos@kopp.com.br>
2024-04-22 17:19:50 -0300

    added  ||  flag that parses the timestamp of the jwt

--- src/main.go
+++ src/main.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"os"
 	"strings"
+	"time"
 )
 
 // token errors
@@ -25,7 +26,21 @@ type Token struct {
 
 func main() {
 	var tokenStr string
-	fileInfo, _ := os.Stdin.Stat()
+	fileInfo, err := os.Stdin.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	timeFlag := false
+	if (len(os.Args) > 1) && (strings.HasPrefix(os.Args[1], "-")) {
+		if os.Args[1] == "-t" || os.Args[1] == "--timestamp" {
+			timeFlag = true
+			os.Args = os.Args[1:]
+		} else {
+			panic(fmt.Errorf("invalid flag: %s", os.Args[1]))
+		}
+	}
+
 	if fileInfo.Mode()&os.ModeCharDevice == 0 {
 		reader := bufio.NewReader(os.Stdin)
 		buffer, err := io.ReadAll(reader)
@@ -69,12 +84,27 @@ func main() {
 		Payload: payload,
 	}
 
-	ba, err := json.MarshalIndent(token, "", "    ")
+	if timeFlag {
+		var timestamp int64 = int64(token.Payload["exp"].(float64))
+		token.Payload["exp"] = fmt.Sprintf(
+			"%d (%s)",
+			timestamp,
+			time.Unix(timestamp, 0).Format("2006-01-02 15:04:05"),
+		)
+		timestamp = int64(token.Payload["iat"].(float64))
+		token.Payload["iat"] = fmt.Sprintf(
+			"%d (%s)",
+			timestamp,
+			time.Unix(timestamp, 0).Format("2006-01-02 15:04:05"),
+		)
+	}
+
+	bytea, err := json.MarshalIndent(token, "", "    ")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(string(ba))
+	fmt.Println(string(bytea))
 }
 
 func breakToken(tokenStr string) ([]string, error) {
